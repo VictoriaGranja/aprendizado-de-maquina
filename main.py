49168c016e125e178ca63b8cffc4aa0e27d11072
VictoriaGranja <victoriagranja@id.uff.br>
2022-10-26 15:59:01 -0300

    pre processamento pontuacao

--- main.py
+++ main.py
@@ -13,18 +13,24 @@ pontuacao = string.punctuation
 nlp = spacy.load("pt_core_news_sm")
 #python -m spacy download pt
 stop_words = set(stopwords.words('portuguese'))
-print(stop_words)
 
 def tokenizando(review):
-    tokens = nlp(review)
-    tokens = [ word.lemma_.lower().strip() for word in tokens ]
+    tokens = nltk.word_tokenize(review)
+    tokens = [word.lower().strip(pontuacao) for word in tokens]
     tokens = [word for word in tokens if word not in stop_words]
-    tokens = [word for word in tokens if word not in pontuacao]
+    tokens = " ".join(tokens)
     return tokens
+
+def lematizacao(review):
+    tokens = nlp(review)
+    tokens = [word.lemma_ for word in tokens]
+    return tokens
+
 
 df = pd.read_csv(r'.\database\olist.csv')
 
-print(df.rating.value_counts())
-
 df['review_tokenizada'] = [tokenizando(review) for review in df.review_text]
+print(df)
+
+df['review_tokenizada'] = [lematizacao(review) for review in df.review_tokenizada]
 print(df)
